989332e7a9be1b3822b2870976fbdbd5bc9ed133
Bob F <bobfdev@gmail.com>
2022-06-19 20:49:28 -0500

    updated files

--- script.js
+++ script.js
@@ -38,3 +38,32 @@ function dragDrop() {
     this.style.border = 'none';
     this.append(draggableTodo);
 }
+
+/* modal */
+
+const btns = document.querySelectorAll('[data-target-modal]');
+const close_modals = document.querySelectorAll('.close-modal');
+const overlay = document.getElementById('overlay');
+
+btns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        document.querySelector(btn.dataset.target).classList.add('active');
+        overlay.classList.add('active');
+    });
+});
+
+close_modals.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        const modal = btn.closest('.modal');
+        modal.classList.remove('active');
+        overlay.classList.remove('active');
+    });
+});
+
+window.onclick = (event) => {
+    if (event.target == overlay) {
+        const modals = document.querySelectorAll('.modal');
+        modals.forEach((modal) => modal.classList.remove('active'));
+        overlay.classList.remove('active');
+    }
+};
